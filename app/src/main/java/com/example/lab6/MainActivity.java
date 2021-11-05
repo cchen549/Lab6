7580690e946827643654be0d2e21d3846032dfa3
Cecheng Chen <cchen549@wisc.edu>
2021-11-04 23:56:03 -0500

    milestone2 and I add code "mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLng, 10))"

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.lab6;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity {
-
-    // Somewhere in Australia
+    // Bascom
     private final LatLng mDestinationLatLng = new LatLng(43.075315,-89.403436);
     private GoogleMap mMap;
 
+    private FusedLocationProviderClient mFusedLocationProviderClient; // Save the instance
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12; // could've been any number!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Obtain a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_map);
         mapFragment.getMapAsync(googleMap -> {
@@ -27,6 +40,27 @@ public class MainActivity extends FragmentActivity {
                 googleMap.addMarker(new MarkerOptions()
                 .position(mDestinationLatLng)
                 .title("Destination"));
+            displayMyLocation();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDestinationLatLng, 10));
         });
+    }
+    private void displayMyLocation(){
+        // Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        // If not, ask for it
+        if (permission==PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // If permission granted, display marker at current location
+        else{
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+                Location mLastKnownLocation = task.getResult();
+                if(task.isSuccessful() && mLastKnownLocation != null){
+                    mMap.addPolyline(new PolylineOptions().add(new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude()),mDestinationLatLng));
+                }
+            });
+        }
     }
 }
